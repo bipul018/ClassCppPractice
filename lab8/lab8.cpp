e181ab56445dba7e441d4273828c5b1747b3cdf7
Bipul Neupane <bipuls2073@gmail.com>
2022-07-28 12:56:17 +0545

    finally done

--- lab8/lab8.cpp
+++ lab8/lab8.cpp
@@ -6,36 +6,78 @@ delete and display the account information (account-number, last-name, first-nam
 */
 #include <iostream>
 #include <fstream>
-
+#include <cstdio>
 namespace Q5 {
-
+	using namespace std;
 	struct Account {
 		char fName[100];
 		char lName[100];
 		unsigned accN;
 		float balance;
+		
+		static unsigned getcount(string filename) {
+			fstream file(filename, ios::in | ios::binary | ios::ate);
+			if (!file.is_open())
+				return 0;
+			unsigned count = file.tellg() / sizeof(Account);
+			file.close();
+			return count;
+		}
+
+		bool readFrom(fstream& file, unsigned position) {
+			if (!file.is_open())
+				return false;
+			file.seekg(0, ios::end);
+			unsigned count = file.tellg() / sizeof(Account);
+			if (position >= count)
+				return false;
+			file.seekg(sizeof(Account) * position, ios::beg);
+			file.read(reinterpret_cast<char*>(this), sizeof(Account));
+			return true;
+		}
+		bool readFrom(string filename, unsigned position) {
+			fstream file(filename, ios::in | ios::ate | ios::binary);
+			bool result = readFrom(file, position);
+			file.close();
+			return result;
+		}
+		bool writeTo(fstream& file, unsigned position) {
+			if (!file.is_open())
+				return false;
+			file.seekg(0, ios::end);
+			unsigned count = file.tellg() / sizeof(Account);
+			if (position > count)
+				position = count;
+			file.seekg(sizeof(Account) * position, ios::beg);
+			file.write(reinterpret_cast<const char*>(this), sizeof(Account));
+			file.close();
+			return true;
+		}
+		bool writeTo(string filename, unsigned position) {
+			fstream file(filename, ios::binary | ios::app | ios::in);
+			bool result = writeTo(file, position);
+			file.close();
+			return result;
+		}
+		void getRecord() {
+
+			cout << "\nAccount Number : ";
+			cin >> accN;
+			cout << "First Name : ";
+			cin >> fName;
+			cout << "Last Name : ";
+			cin >> lName;
+			cout << "Balance : ";
+			cin >> balance;
+		}
 	};
+
 	using namespace std;
 	int main() {
 		string fileName;
-		unsigned count = 0;
-		Account* pacc = nullptr;
 		cout << "Enter the file name ";
 		cin >> fileName;
 
-		fstream file(fileName, ios::in | ios::ate | ios::binary);
-		if (file.is_open()) {
-			count = file.tellg() / sizeof(Account);
-			file.seekg(ios::beg);
-			pacc = new Account[count];
-			file.read(reinterpret_cast<char*>(pacc), count * sizeof(Account));
-			file.close();
-		}
-		else {
-			count = 0;
-			file.open(fileName, ios::binary | ios::out);
-			file.close();
-		}
 		char choice = 'c';
 
 		cout << "Enter your choice as follow : "
@@ -50,11 +92,19 @@ namespace Q5 {
 			switch (choice) {
 			case 'd':
 			{
+
+				unsigned count = Account::getcount(fileName);
+
+				fstream file(fileName, ios::binary | ios::in);
+
 				for (unsigned n = 0; n < count; n++) {
-					std::cout << "\nRecord No :" << n + 1 << "\n\tName:" << pacc[n].fName << " " << pacc[n].lName
-						<< "\n\tAcc No : " << pacc[n].accN
-						<< "\n\tBalance : " << pacc[n].balance << endl;
+					Account acc;
+					acc.readFrom(file, n);
+					std::cout << "\nRecord No :" << n + 1 << "\n\tName:" << acc.fName << " " << acc.lName
+						<< "\n\tAcc No : " << acc.accN
+						<< "\n\tBalance : " << acc.balance << endl;
 				}
+				file.close();
 			}
 				break;
 			case 'r':
@@ -62,17 +112,29 @@ namespace Q5 {
 				cout << "Enter record number to remove : ";
 				int n;
 				cin >> n;
-				if ((n > count)||(n<=0)) {
+
+				unsigned count = Account::getcount(fileName);
+				if ((n > Account::getcount(fileName) || (n <= 0))) {
 					cout << "Invalid Record.\n";
 					break;
 				}
+				for (unsigned i = 0; i < count; i++) {
+					Account acc;
+					if (i < (n-1)) {
+						acc.readFrom(fileName, i);
+						acc.writeTo("tempfile", i);
+					}
+					else if (i > (n-1)) {
+						acc.readFrom(fileName, i );
+						acc.writeTo("tempfile", i-1);
+					}
+				}
 
-				Account* newp = new Account[count - 1];
-				memcpy(newp, pacc, sizeof(Account)*(n-1));
-				memcpy(newp + n - 1, pacc + n, sizeof(Account) * (count - n ));
-				delete[] pacc;
-				pacc = newp;
-				--count;
+				remove(fileName.c_str());
+				rename("tempfile", fileName.c_str());
+				remove("tempfile");
+
+
 
 			}
 			break;
@@ -81,19 +143,27 @@ namespace Q5 {
 				cout << "Enter record number to update : ";
 				int n;
 				cin >> n;
+				unsigned count = Account::getcount(fileName);
 				if ((n > count) || (n <= 0)) {
 					cout << "Invalid Record.\n";
 					break;
 				}
-				cout << "\nEnter record to update : ";
-				cout << "\nAccount Number : ";
-				cin >> pacc[n - 1].accN;
-				cout << "\nFirst Name : ";
-				cin >> pacc[n - 1].fName;
-				cout << "\nLast Name : ";
-				cin >> pacc[n - 1].lName;
-				cout << "\nBalance : ";
-				cin >> pacc[n - 1].balance;
+				for (unsigned i = 0; i < count; i++) {
+					Account acc;
+					if (i== (n - 1)) {
+						cout << "\nEnter record to update : ";
+						acc.getRecord();
+						acc.writeTo("tempfile", i);
+					}
+					else {
+						acc.readFrom(fileName, i);
+						acc.writeTo("tempfile", i);
+					}
+				}
+
+				remove(fileName.c_str());
+				rename("tempfile", fileName.c_str());
+				remove("tempfile");
 
 			}
 			break;
@@ -102,43 +172,38 @@ namespace Q5 {
 				cout << "Enter record number to add after : ";
 				int n;
 				cin >> n;
+				unsigned count = Account::getcount(fileName);
 				if (n > count)
 					n = count;
-
 				if (n <= 0)
 					n = 0;
-				Account* newp = new Account[count + 1];
-				if (pacc != nullptr)
-					memcpy(newp, pacc, sizeof(Account) * n);
 
-				cout << "\nEnter record to add : ";
-				cout << "\nAccount Number : ";
-				cin >> newp[n].accN;
-				cout << "\nFirst Name : ";
-				cin >> newp[n].fName;
-				cout << "\nLast Name : ";
-				cin >> newp[n].lName;
-				cout << "\nBalance : ";
-				cin >> newp[n].balance;
+				for (unsigned i = 0; i < count+1; i++) {
+					Account acc;
+					if (i < (n )) {
+						acc.readFrom(fileName, i);
+						acc.writeTo("tempfile", i);
+					}
+					else if (i == n) {
 
-				if (pacc != nullptr)
-					memcpy(newp + n + 1, pacc + n, sizeof(Account) * (count - n));
-				delete[] pacc;
-				pacc = newp;
-				++count;
+						cout << "\nEnter record to add : ";
+						acc.getRecord();
+						acc.writeTo("tempfile", i);
+					}
+					else if (i > (n)) {
+						acc.readFrom(fileName, i-1);
+						acc.writeTo("tempfile", i + 1);
+					}
+				}
 
-
+				remove(fileName.c_str());
+				rename("tempfile", fileName.c_str());
+				remove("tempfile");
+			
 			}
 				break;
 			}
-			file.open(fileName, ios::binary | ios::out);
-			file.write(reinterpret_cast<const char*>(pacc), count);
-			file.close();
 		}
-
-
-
-		delete[] pacc;
 		return 0;
 	}
 }
